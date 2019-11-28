8923b40be479018ed4dfb4ef2dd2aa7bbff205a0
ChrisAchinga <achinga.chris@gmail.com>
2019-11-28 17:15:15 +0300

    styles admin page

--- polls/admin.py
+++ polls/admin.py
@@ -7,6 +7,8 @@ class ChoiceInline(admin.StackedInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,               {'fields': ['question_text']}),
